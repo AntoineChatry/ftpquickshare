3de69b7f055b371e1dbac60efa46a4871a2abc73
AntoineChatry <64854295+AntoineChatry@users.noreply.github.com>
2024-02-19 22:28:03 +0100

    Update context_menu.py
    Now handles folders and .zip files

--- context_menu.py
+++ context_menu.py
@@ -9,25 +9,29 @@ python_exe = sys.executable
 # optional hide python terminal in windows
 hidden_terminal = "\\".join(python_exe.split("\\")[:-1]) + "\\pythonw.exe"
 
-# Set the path of the context menu (right-click menu)
-key_path = r"*\shell\\SendToFTP\\"
+# Set the paths of the context menu (right-click menu)
+key_paths = [
+    r"*\shell\SendToNAS",
+    r"Directory\shell\SendToFTP",
+    r"CompressedFolder\shell\SendToFTP",
+]
 
-try:
-    reg.DeleteKey(reg.HKEY_CLASSES_ROOT, key_path + r"\command")
-    reg.DeleteKey(reg.HKEY_CLASSES_ROOT, key_path)
-except FileNotFoundError:
-    pass
+for key_path in key_paths:
+    try:
+        reg.DeleteKey(reg.HKEY_CLASSES_ROOT, key_path + r"\command")
+        reg.DeleteKey(reg.HKEY_CLASSES_ROOT, key_path)
+    except FileNotFoundError:
+        pass
 
-# Create outer key
-key = reg.CreateKey(reg.HKEY_CLASSES_ROOT, key_path)
-reg.SetValue(key, "", reg.REG_SZ, "&Send to FTP")
+    # Create outer key
+    key = reg.CreateKey(reg.HKEY_CLASSES_ROOT, key_path)
+    reg.SetValue(key, "", reg.REG_SZ, "&Send to FTP")
 
-# create inner key
-key1 = reg.CreateKey(key, r"command")
-reg.SetValue(
-    key1,
-    "",
-    reg.REG_SZ,
-    r'yourpythonpath main.pypath "%1"',
-)  
-# reg.SetValue(key1, '', reg.REG_SZ, hidden_terminal + f' "{cwd}\\main.py%1"')  # use to to hide terminal
+    # create inner key
+    key1 = reg.CreateKey(key, r"command")
+    reg.SetValue(
+        key1,
+        "",
+        reg.REG_SZ,
+        r'yourpythonpath main.pypath "%1"',
+    )
